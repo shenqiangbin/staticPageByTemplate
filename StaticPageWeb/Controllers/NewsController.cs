a186257df68a6660a573e9dca312dbecd012b3ed
sks <sks@SKS-PC>
2017-03-29 23:16:10 +0800

    生成静态页根据模版文件

---
+++ StaticPageWeb/Controllers/NewsController.cs
@@ -0,0 +1,65 @@
+﻿using StaticPageWeb.Models;
+using StaticPageWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticPageWeb.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            NewsService service = new NewsService();
+            List<News> news = service.Get();
+
+            return View(news);
+        }
+
+        public ActionResult BuildStaticFiles()
+        {
+            NewsService service = new NewsService();
+            List<News> news = service.Get();
+
+            string newsTemplateFileContent = GetNewsTemplate();
+            foreach (var item in news)
+            {
+                string replacedContent = ReplaceTemplateFileByNewsModel(newsTemplateFileContent, item);
+                SaveStaticFile(replacedContent,item);
+            }
+
+            return Content("静态文件生成完成");
+        }
+
+        private string GetNewsTemplate()
+        {
+            string templateFile = Server.MapPath("~/Templates/NewsTemplate.html");
+            return System.IO.File.ReadAllText(templateFile);
+        }
+
+        private string ReplaceTemplateFileByNewsModel(string newsTemplateFileContent, News item)
+        {
+            return
+                newsTemplateFileContent.
+                Replace("@title", item.Title)
+                .Replace("@author", item.Author)
+                .Replace("@releaseTime", item.ReleaseTime.ToString("yyyy-MM-dd HH:mm:ss"))
+                .Replace("@content", item.Content);
+        }
+
+
+        private void SaveStaticFile(string replacedContent, News model)
+        {
+            string directoryPath = Server.MapPath("~/Statics/News");
+            System.IO.Directory.CreateDirectory(directoryPath);
+
+            string filePath = $"{directoryPath}/{model.Id}.html";
+            System.IO.File.WriteAllText(filePath, replacedContent);
+        }
+
+       
+    }
+}
---
+++ StaticPageWeb/Services/NewsService.cs
@@ -0,0 +1,49 @@
+﻿using StaticPageWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StaticPageWeb.Services
+{
+    public class NewsService
+    {
+        private List<News> _list;
+
+        public NewsService()
+        {
+            _list = new List<News>();
+            _list.Add(new News
+            {
+                Id = 1,
+                Author = "小编1",
+                ReleaseTime = new DateTime(1991, 1, 1, 13, 12, 31),
+                Title = "新闻标题1",
+                Content = "新闻内容1"
+            });
+
+            _list.Add(new News
+            {
+                Id = 2,
+                Author = "小编2",
+                ReleaseTime = new DateTime(2001, 2, 3, 13, 12, 31),
+                Title = "新闻标题2",
+                Content = "新闻内容2"
+            });
+
+            _list.Add(new News
+            {
+                Id = 3,
+                Author = "小编3",
+                ReleaseTime = new DateTime(2003, 2, 3, 13, 12, 31),
+                Title = "新闻标题3",
+                Content = "新闻内容3"
+            });
+        }
+
+        public List<News> Get()
+        {
+            return _list;
+        }
+    }
+}
